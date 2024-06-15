80c60d3f57bcbd0b7905023225f0445892e0cf19
magdaTrzTrash <mwt.trash@gmail.com>
2024-06-15 22:20:17 +0200

    umowa brokierska, raport, logika

---
+++ models/umo/brookerage_agreement.py
@@ -0,0 +1,128 @@
+import os
+import numpy
+import pandas
+
+from pydispatch import dispatcher
+from controllers.progress_bar import ProgresBarStatus
+from models.excel_report import ExcelReport
+from models.koi import dict_KOI
+from models.report_model import ReportModel
+from text_variables import TextEnum
+
+UPDATE_TEXT_SIGNAL = 'update_text'
+
+
+class UmoBrokerage(ReportModel):
+    def __init__(self, stage: str, path_src=None, path_ext=None, path_tgt=None, data_folder_report_path='',
+                 save_folder_report_path='', path_excel_file='report.xlsx', password=None):
+        print('UmoBrokerage')
+        super().__init__()
+        self.stage = stage
+        self.path_src = path_src
+        self.path_ext = path_ext
+        self.path_tgt = path_tgt
+        self.data_folder_report_path: str = data_folder_report_path
+        self.save_folder_report_path: str = save_folder_report_path
+        self.path_excel: str = path_excel_file
+        self.dataframe_src: pandas.DataFrame | None = None
+        self.dataframe_ext: pandas.DataFrame | None = None
+        self.dataframe_tgt: pandas.DataFrame | None = None
+        self.summary_dataframe: pandas.DataFrame | None = None
+        self.merge_statistics_dataframe: pandas.DataFrame | None = None
+        self.percent_reconciliation_dataframe: pandas.DataFrame | None = None
+        self.sample_dataframe: pandas.DataFrame | None = None
+        self.password: None | str = password
+
+    def _carry_operations(self) -> bool:
+        print(f'UmoBrokerage: _carry_operations(stage={self.stage})')
+
+        ext_dataframe = self.make_dataframe_from_file(self.path_ext, self.data_folder_report_path)
+        ext_dataframe = self.set_colum_names({0: "numer_umowy", 1: "numer_klienta", 2: 'typ_atrybutu', 3: 'wartosc'},
+                                             ext_dataframe)
+        if self.stage == TextEnum.LOAD:
+            src_dataframe = self.make_dataframe_from_file(self.path_src, self.data_folder_report_path)
+            src_dataframe = self.set_colum_names({0: "numer_umowy", 1: "numer_klienta"}, src_dataframe)
+
+            if src_dataframe.empty or ext_dataframe.empty:
+                return False
+            else:
+                self.dataframe_src = self.convert_src(src_dataframe)
+                self.dataframe_ext = self.convert_ext(ext_dataframe)
+                ProgresBarStatus.increase()
+                return True
+
+        if self.stage == TextEnum.END:
+            tgt_dataframe = self.make_dataframe_from_file(self.path_tgt, self.data_folder_report_path)
+            tgt_dataframe = self.set_colum_names({0: "numer_umowy", 1: "numer_klienta", 2: 'typ_atrybutu', 3: 'wartosc'}, tgt_dataframe)
+
+            if ext_dataframe.empty or tgt_dataframe.empty:
+                return False
+            else:
+                self.dataframe_ext = self.convert_ext(ext_dataframe)
+                self.dataframe_tgt = self.convert_tgt(tgt_dataframe)
+                ProgresBarStatus.increase()
+                return True
+
+    @staticmethod
+    def convert_src(dataframe: pandas.DataFrame) -> pandas.DataFrame:
+        ProgresBarStatus.increase()
+        return dataframe[["numer_umowy", "numer_klienta"]]
+
+    @staticmethod
+    def convert_ext(dataframe: pandas.DataFrame) -> pandas.DataFrame:
+        ProgresBarStatus.increase()
+        return dataframe[["numer_umowy", "numer_klienta"]]
+
+    @staticmethod
+    def convert_tgt(dataframe: pandas.DataFrame) -> pandas.DataFrame:
+        ProgresBarStatus.increase()
+        return dataframe[["numer_umowy", "numer_klienta"]]
+
+    def create_report(self) -> TextEnum | bool:
+        self.path_excel = self.modify_filename(self.path_excel, self.stage)
+        print(f"UmoBrokerage(): create_report({self.path_excel}  {self.save_folder_report_path})")
+        try:
+            path_to_excel_file = os.path.join(self.save_folder_report_path, self.path_excel)
+            excel_workbook = ExcelReport(path_to_excel_file, self.stage)
+        except Exception as e:
+            print(f"UmoBrokerage(): create_report  Error tworzenia excela : {e}")
+            return TextEnum.EXCEL_ERROR
+
+        try:
+            if self.stage == TextEnum.LOAD:
+                f2f = excel_workbook.create_f2f_report(
+                    dataframe_1=self.dataframe_src,
+                    dataframe_2=self.dataframe_ext,
+                    merge_on_cols=["numer_umowy"],
+                    compare_cols=["numer_klienta"],
+                    text_description="Rejestr umów maklerskich")
+            elif self.stage == TextEnum.END:
+                f2f = excel_workbook.create_f2f_report(
+                    dataframe_1=self.dataframe_ext,
+                    dataframe_2=self.dataframe_tgt,
+                    merge_on_cols=["numer_umowy"],
+                    compare_cols=["numer_klienta"],
+                    text_description="Rejestr umów maklerskich")
+            ProgresBarStatus.increase()
+            self.summary_dataframe = excel_workbook.summary_dataframe
+            self.merge_statistics_dataframe = excel_workbook.merge_statistics_dataframe
+            self.percent_reconciliation_dataframe = excel_workbook.percent_reconciliation_dataframe
+            self.sample_dataframe = excel_workbook.sample_dataframe
+        except Exception as e:
+            print(f"UmoBrokerage(): create_report  Error tworzenia raportu : {e}")
+            dispatcher.send(signal=UPDATE_TEXT_SIGNAL, message=f"Błąd tworzenia raportu {e}", head='error')
+            return TextEnum.CREATE_ERROR
+
+        try:
+            excel_workbook.save_to_excel({f"f2f_brokerage_agreement": f2f}, merge_on=["numer_umowy"])
+        except Exception as e:
+            print(f"UmoBrokerage(): create_report  Error zapisywania raportu : {e}")
+            dispatcher.send(signal=UPDATE_TEXT_SIGNAL, message=f"Błąd zapisywania raportu {e}", head='error')
+            return TextEnum.SAVE_ERROR
+
+        try:
+            excel_workbook.add_password_to_excel(self.path_excel, self.password)
+        except Exception as e:
+            print(f"UmoBrokerage(): add_password_to_excel  Error daodawania hasła do raportu : {e}")
+            dispatcher.send(signal=UPDATE_TEXT_SIGNAL, message=f"Błąd dodawania hasła do raportu {e}", head='error')
+        return True
